db69254d0e3826d7f4f4b4ab25e5bf6e6de76c55
louisleedev <bj.louislee@gmail.com>
2023-12-13 23:20:28 -0500

    Basic Implement of Rapid API

---
+++ src/redux/services/shazamCore.js
@@ -0,0 +1,18 @@
+import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
+
+const options = {
+    method: 'GET',
+    headers: {
+      'X-RapidAPI-Key': 'fcdf1e7caamsh0caa50e294e8fbep17def7jsnc98460de6e79',
+      'X-RapidAPI-Host': ''
+    }
+  };
+  
+  fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+    .then(response => response.json())
+    .then(response => console.log(response))
+    .catch(err => console.error(err));
+
+  export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi'
+  })
