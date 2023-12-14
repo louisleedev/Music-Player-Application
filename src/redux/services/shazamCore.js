182e158c1f4a239e1912652ba61ee8c9e8d27e39
louisleedev <bj.louislee@gmail.com>
2023-12-14 17:45:38 -0500

    Implement Rapid API and fetching

--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -3,16 +3,31 @@ import { createApi, fetchBaseQuery } from '@reduxjs/toolkit/query/react'
 const options = {
     method: 'GET',
     headers: {
-      'X-RapidAPI-Key': 'fcdf1e7caamsh0caa50e294e8fbep17def7jsnc98460de6e79',
-      'X-RapidAPI-Host': ''
+        'X-RapidAPI-Key': 'fcdf1e7caamsh0caa50e294e8fbep17def7jsnc98460de6e79',
+        'X-RapidAPI-Host': ''
     }
-  };
-  
-  fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
+};
+
+fetch('https://shazam-core.p.rapidapi.com/v1/charts/world', options)
     .then(response => response.json())
     .then(response => console.log(response))
     .catch(err => console.error(err));
 
-  export const shazamCoreApi = createApi({
-    reducerPath: 'shazamCoreApi'
-  })
+export const shazamCoreApi = createApi({
+    reducerPath: 'shazamCoreApi',
+    baseQuery: fetchBaseQuery({
+        baseUrl: 'https://shazam-core.p.rapidapi.com/v1',
+        prepareHeaders: (headers) => {
+            headers.set('X-RapidAPI-Key', 'fcdf1e7caamsh0caa50e294e8fbep17def7jsnc98460de6e79');
+
+            return headers;
+        },
+    }),
+    endpoints: (builder) => ({
+        getTopCharts: builder.query({ query: () => '/charts/world' })
+    })
+});
+
+export const {
+    useGetTopChartsQuery
+} = shazamCoreApi;
