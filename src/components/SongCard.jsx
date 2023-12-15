ded1a838c23a0468b5b8c54b51ce365c102c814f
louisleedev <bj.louislee@gmail.com>
2023-12-15 18:22:36 -0500

    Implemented Player Successfully

--- src/components/SongCard.jsx
+++ src/components/SongCard.jsx
@@ -5,24 +5,26 @@ import PlayPause from './PlayPause';
 import { playPause, setActiveSong } from '../redux/features/playerSlice';
 
 const SongCard = ({ song, isPlaying, activeSong, data, i }) => {
+  const dispatch = useDispatch();
   const handlePauseClick = () => {
-
+    dispatch(playPause(false));
   }
 
   const handlePlayCLick = () => {
-
+    dispatch(setActiveSong({ song, data, i }));
+    dispatch(playPause(true))
   }
 
   return (
     <div className='flex flex-col w-[250px] p-4 bg-white/5 bg-opacity-80 backdrop-blur-sm animate-slideup rounded-lg cursor-pointer'>
       <div className='relative w-full h-56 group'>
         <div className={`absolute inset-0 justify-center items-center bg-black bg-opacity-50 group-hover:flex ${activeSong?.title === song.title ? 'flex bg-black bg-opacity-70' : 'hidden'}`}>
-          <PlayPause 
+          <PlayPause
             isPlaying={isPlaying}
             activeSong={activeSong}
             song={song}
             handlePause={handlePauseClick}
-            handlePlay={handlePlayCLick}/>
+            handlePlay={handlePlayCLick} />
         </div>
         <img alt='song_img' src={song.images?.coverart} />
       </div>
