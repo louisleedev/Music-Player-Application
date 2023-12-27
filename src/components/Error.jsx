d4839cf1f9b0958fd25d9b896c1659658438330d
louisleedev <bj.louislee@gmail.com>
2023-12-26 23:09:17 -0500

    .

--- src/components/Error.jsx
+++ src/components/Error.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Error = () => (
   <div className='w-full flex justify-center items-center'>
-    <h1 className='font-bold text-2xl text-white mt-2'>Oops... Something went wrong...</h1>
+    <h1 className='font-bold text-2xl text-white mt-2'>Oops... Something went wrong...!</h1>
   </div>
 );
 
