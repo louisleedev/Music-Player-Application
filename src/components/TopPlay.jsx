c4ea4a652841ea40b2b3a9db9d784bde222a124d
louisleedev <bj.louislee@gmail.com>
2023-12-29 02:00:42 -0500

    Top Charts function implemented

--- src/components/TopPlay.jsx
+++ src/components/TopPlay.jsx
@@ -11,7 +11,7 @@ import { useGetTopChartsQuery } from "../redux/services/shazamCore";
 import 'swiper/css'
 import 'swiper/css/free-mode'
 
-const TopChartCard = ({ song, i }) => (
+const TopChartCard = ({ song, i, isPlaying, activeSong, handlePauseClick, handlePlayCLick }) => (
   <div className="w-full flex flex-row items-center hover:bg-[#4c426e] py-2 p-4 rounded-lg cursor-pointer mb-2">
     <h3 className="fot-bold text-base text-white mr-3">
       {i + 1}.
@@ -27,6 +27,12 @@ const TopChartCard = ({ song, i }) => (
         </Link>
       </div>
     </div>
+    <PlayPause
+      isPlaying={isPlaying}
+      activeSong={activeSong}
+      song={song}
+      handlePause={handlePauseClick}
+      handlePlay={() => handlePlayCLick(song, i)} />
   </div>
 )
 
@@ -46,7 +52,7 @@ const TopPlay = () => {
     dispatch(playPause(false));
   }
 
-  const handlePlayCLick = () => {
+  const handlePlayCLick = (song, i) => {
     dispatch(setActiveSong({ song, data, i }));
     dispatch(playPause(true))
   }
@@ -70,7 +76,11 @@ const TopPlay = () => {
             <TopChartCard
               key={song.key}
               song={song}
-              i={i} />
+              i={i}
+              isPlaying={isPlaying}
+              activeSong={activeSong}
+              handlePauseClick={handlePauseClick}
+              handlePlayCLick={handlePlayCLick} />
           ))}
         </div>
       </div>
